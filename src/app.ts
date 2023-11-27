c17f1f0d2b74d3375502c8591c42f361fabc53b8
danggro <anangdwin55@gmail.com>
2023-11-27 16:35:15 +0700

    init template express typescript

---
+++ src/app.ts
@@ -0,0 +1,12 @@
+import express from 'express'
+const app = express()
+
+const add = (a: number, b: number): number => a + b
+
+app.get('/', (req, res) => {
+  const result = add(2, 2)
+  res.send(`2 plus 2 is ${result}`)
+})
+
+const PORT = 3000
+app.listen(3000, () => console.log(`Server running on port ${PORT}`))

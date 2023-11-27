45ef4b8dedcc2e2841973adf9a36e2ddcd2739d0
danggro <anangdwin55@gmail.com>
2023-11-27 20:30:10 +0700

    add lint & change config

--- src/app.ts
+++ src/app.ts
@@ -3,7 +3,7 @@ const app = express()
 
 const add = (a: number, b: number): number => a + b
 
-app.get('/', (req, res) => {
+app.get('/', (_req, res) => {
   const result = add(2, 2)
   res.send(`2 plus 2 is ${result}`)
 })
